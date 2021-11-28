e4b1b0d5c6eb65153c04c5f2359a2637aed40277
joseRiver39 <joseantonior4@hotmail.es>
2021-11-27 20:24:48 -0500

    actualizacion

--- frontend/server.js
+++ frontend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/frontend'));
+app.use(express.static(__dirname + '/dist/fron'));
 app.use(cors());
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
